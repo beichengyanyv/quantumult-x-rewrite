8e283052e92e0406c23993adb8cdc3090201d914
beichengyanyv <111874678+beichengyanyv@users.noreply.github.com>
2023-03-12 20:06:54 +0800

    Update and rename lanjiyin.js to lanjiyin.conf

--- lanjiyin.js
+++
@@ -1,5 +0,0 @@
-var body = $request.body;
-body = body.replace(/is_free\\\":\\\"0\\\"/, "is_free\\\":\\\"1\\\"");
-body = body.replace(/is_see\\\":\\\"0\\\"/, "is_see\\\":\\\"1\\\"");
-body = body.replace(/is_unlock\\\":\\\"0\\\"/, "is_unlock\\\":\\\"1\\\"");
-$done({body: body});
