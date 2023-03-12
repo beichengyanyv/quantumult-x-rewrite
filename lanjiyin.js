880dc294156209122f715a34c93148d89ba75b85
beichengyanyv <111874678+beichengyanyv@users.noreply.github.com>
2023-03-12 20:13:32 +0800

    Update lanjiyin.js

--- lanjiyin.js
+++ lanjiyin.js
@@ -1,7 +1,29 @@
-var body = $response.body;
-
-body = body.replace(/"is_free":"0"/g, '"is_free":"1"');
-body = body.replace(/"is_see":"0"/g, '"is_see":"1"');
-body = body.replace(/"is_unlock":"0"/g, '"is_unlock":"1"');
-
-$done({ body });
+[
+  {
+    "type": "field",
+    "pattern": "*.lanjiyin.com.cn",
+    "method": "ALL",
+    "destionation": "DIRECT",
+    "rewrite": {
+      "request": [
+        {
+          "type": "body",
+          "match": "is_free\":\"0\"",
+          "replace": "is_free\":\"1\""
+        },
+        {
+          "type": "body",
+          "match": "is_see\":\"0\"",
+          "replace": "is_see\":\"1\""
+        }
+      ],
+      "response": [
+        {
+          "type": "body",
+          "match": "is_unlock\":\"0\"",
+          "replace": "is_unlock\":\"1\""
+        }
+      ]
+    }
+  }
+]
